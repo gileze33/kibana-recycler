fd8f005dbd00c3f82769281f9fdc3014f119965e
Giles MM <giles.williams@gmail.com>
2015-04-01 13:41:43 +0000

    Fixed directories for bin script

--- run.js
+++ run.js
@@ -14,7 +14,7 @@ var async = require('async');
 
 require('colors');
 
-var Pattern = require('./lib/pattern');
+var Pattern = require(__dirname + '/lib/pattern');
 
 var patterns = [];
 for(var i=0; i<config.patterns.length; i++) {

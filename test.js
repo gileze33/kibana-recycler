fa1b8866e9427b39bcad72d93fd8f465a8d311da
Giles Williams <giles@urbanmassage.com>
2015-04-01 10:26:12 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var path = require('path');
+var configStr = fs.readFileSync(path.join(__dirname, 'config.json'));
+var config = JSON.parse(configStr);
+
+var async = require('async');
+
+require('colors');
+
+//console.log(config);
+
+var Pattern = require('./lib/pattern');
+
+var patterns = [];
+for(var i=0; i<config.patterns.length; i++) {
+	patterns.push(new Pattern(config.patterns[i]));
+}
+
+async.each(patterns, function(pattern, callback) {
+	pattern.cleanup(callback);
+}, function(err) {
+	if(err) {
+		console.log('ERROR:'.red, err);
+		process.exit(1);
+	}
+
+	console.log(('\nCompleted successfully for '+patterns.length+' patterns').green);
+	process.exit(0);
+});
